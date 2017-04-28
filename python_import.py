331f3167b19f4b580f355027b7d18f7f1d85a558
暁东邪 <xiaodongxiexie@users.noreply.github.com>
2017-04-28 17:31:31 +0800

    Update python_import.py

--- python_import.py
+++ python_import.py
@@ -3,10 +3,20 @@ import sys
 import re
 import codecs
 import tarfile
+import pickle
+import cPickle
+import functools
 import datetime
 import time
 import collections
 import dateutil
+import itertools
+import warnings
+import copy
+import random
+import csv
+import math
+
 import numpy 
 import numpy as np
 import pandas
@@ -14,12 +24,30 @@ import pandas as pd
 import scipy
 import scipy as sp
 import matplotlib.pyplot as plt
-import itertools
+import talib
+import pybrain
+import sklearn
+import statsmodels
+
+
+warnings.filterwarnings('ignore')
+
+
+from __future__ import division
+
+from copy import deepcopy
 
 from matplotlib import pyplot as plt
 
 from numpy import random
-from numpy import polyfit
+from numpy import polyfit, std, subtract, sqrt, log
+
+from scipy.misc import derivative  #求导数
+
+from pandas import Series, DataFrame
+
+from functools import partial
+from functools import wraps
 
 from collections import Counter
 from collections import defaultdict
@@ -49,3 +77,21 @@ from itertools import (chain,
                         repeat,
                         starmap,
                         takewhile)
+
+
+from statsmodels.tsa.arima_model import ARMA, ARIMA
+from statsmodels.tsa.stattools import adfuller, acf, pacf
+
+
+
+from pybrain.tools.shortcuts import buildNetwork
+from pybrain.structure import FeedForwardNetwork, LinearLayer, SigmoidLayer, TanhLayer, SoftmaxLayer, FullConnection
+from pybrain.supervied.trainers import BackpropTrainer
+from pybrain.datasets import SupervisedDataSet
+from pybrain.tools.customxml.networkwriter import NetworkWriter
+from pybrain.tools.customxml.networkreader import NetworkReader
+
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.externals import joblib
+
+
