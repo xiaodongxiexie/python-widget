0a2db9f42c763e3ea8ef2b60332179a246605749
暁东邪 <xiaodongxiexie@users.noreply.github.com>
2017-04-28 17:16:50 +0800

    python import

---
+++ python_import.py
@@ -0,0 +1,51 @@
+import os
+import sys
+import re
+import codecs
+import tarfile
+import datetime
+import time
+import collections
+import dateutil
+import numpy 
+import numpy as np
+import pandas
+import pandas as pd
+import scipy
+import scipy as sp
+import matplotlib.pyplot as plt
+import itertools
+
+from matplotlib import pyplot as plt
+
+from numpy import random
+from numpy import polyfit
+
+from collections import Counter
+from collections import defaultdict
+from collections import OrderedDict
+from collections import deque
+
+from datetime import datetime
+from datetime import timedelta
+
+
+from dateutil.parser import parse
+
+from itertools import (chain,
+                        compress, 
+                        count, 
+                        cycle, 
+                        dropwhile, 
+                        ifilter, 
+                        ifilterfalse, 
+                        imap,
+                        islice,
+                        imap,
+                        izip,
+                        izip_longest,
+                        permutations,
+                        product,
+                        repeat,
+                        starmap,
+                        takewhile)
