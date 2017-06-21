cc8d287ed5776e2da8981fdb6641786ca18392a0
暁东邪 <xiaodongxiexie@users.noreply.github.com>
2017-06-21 15:59:56 +0800

    Update searchAllFile.py

--- searchAllFile.py
+++ searchAllFile.py
@@ -1,10 +1,17 @@
 #coding: utf-8
 
-
+#使用递归查找指定目录下所有文件，并美化输出
 import os
 import glob
-def searchFile(root_path):
+def searchFile(root_path,flag=0):
     if glob.glob(os.path.join(root_path, '*')):
         for x in glob.glob(os.path.join(root_path, '*')):
-            print x  
-            searchFile(x)
+            if flag >= 1:
+                print '\t'*flag, x
+            else:
+                print x
+            flag += 1
+            searchFile(x, flag=flag)
+
+if __name__ == '__main__':
+    searchFile('.')
