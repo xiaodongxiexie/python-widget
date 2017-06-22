e6af1ab42e1fc391fb9cdf30564f5337bf9f8c26
暁东邪 <xiaodongxiexie@users.noreply.github.com>
2017-06-22 23:02:37 +0800

    Update clear_screen.py

--- clear_screen.py
+++ clear_screen.py
@@ -3,7 +3,7 @@
 
 import os
 
-def clear_screen:
+def clear_screen():
     if os.name == "posix":		# Unix/Linux/MacOS/BSD/etc
         os.system('clear')		# Clear the Screen
     elif os.name in ("nt", "dos", "ce"):	# DOS/Windows
