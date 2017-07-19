f9c3bc4e1bfef8c2e2ef900cbf32750cd7f21df6
暁东邪 <xiaodongxiexie@users.noreply.github.com>
2017-07-19 15:02:34 +0800

    Create bubble_sort.py

---
+++ algorithms/bubble_sort.py
@@ -0,0 +1,20 @@
+#coding: utf-8
+
+
+def bubble_sort(seq):
+    length = len(seq)
+    for i in range(length-1, -1, -1):
+        for j in range(i):
+            if seq[j] > seq[j+1]:
+                seq[j], seq[j+1] = seq[j+1], seq[j]
+    return seq
+    
+    
+#
+def bb(seq):
+    length = len(seq)
+    for i in range(length):
+        for j in range(i):
+            if seq[j] > seq[j+1]:
+                seq[j+1], seq[j] = seq[j], seq[j+1]
+    return seq
