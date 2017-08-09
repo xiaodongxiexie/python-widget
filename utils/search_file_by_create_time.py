967e411206fe0fd8f29773f587d79f6d90cacd8d
暁东邪 <xiaodongxiexie@users.noreply.github.com>
2017-08-09 14:47:48 +0800

    Update search_file_by_create_time.py

--- utils/search_file_by_create_time.py
+++ utils/search_file_by_create_time.py
@@ -1,4 +1,6 @@
 #coding: utf-8
 
+import os, glob, time
+
 def search_all_files_return_by_time_reversed(path, reverse=True):
 	return sorted(glob.glob(os.path.join(path, '*')), key=lambda x: time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(os.path.getctime(x))), reverse=reverse)
