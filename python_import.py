9b4ade9c7b4bd9e64320939c3a2bd25914bb6c50
暁东邪 <xiaodongxiexie@users.noreply.github.com>
2017-08-12 14:40:17 +0800

    Update python_import.py

--- python_import.py
+++ python_import.py
@@ -290,6 +290,8 @@ from collections import Counter
 from collections import defaultdict
 from collections import OrderedDict
 from collections import deque
+from collections import namedtuple
+from collections import ChainMap
 
 from copy import deepcopy
 
