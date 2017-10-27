bb3a5842f13f79e089ba5faf44049a3b03f0558b
liangxiaodong <1027887088@qq.com>
2017-10-27 17:19:51 +0800

    计算欧几里得距离，相似度

---
+++ algorithms/calc_similarity.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Author: xiaodong
+# @Date:   2017-10-27 17:17:14
+# @Last Modified by:   xiaodong
+# @Last Modified time: 2017-10-27 17:19:19
+
+#计算距离
+def calc_distance(seq1, seq2):
+    return sum(map(lambda x, y: ((x-y)**2)**0.5, seq1, seq2))
+
+#计算相似度
+def calc_similarity(seq1, seq2):
+    return 1/(1+sum(map(lambda x, y: ((x-y)**2)**0.5, seq1, seq2)))
