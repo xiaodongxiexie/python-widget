d8b59b417bd72705199cebd05e53bf656871764e
暁东邪 <xiaodongxiexie@users.noreply.github.com>
2017-10-28 09:15:49 +0800

    Update calc_similarity.py

--- algorithms/calc_similarity.py
+++ algorithms/calc_similarity.py
@@ -11,3 +11,8 @@ def calc_distance(seq1, seq2):
 #计算相似度
 def calc_similarity(seq1, seq2):
     return 1/(1+sum(map(lambda x, y: ((x-y)**2)**0.5, seq1, seq2)))
+
+if __name__ == '__main__':
+    x = range(5)
+    y = range(5, 10)
+    print (calc_similarity(x, y))
