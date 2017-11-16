6e2f8e28013a581b25785b98f96a7b73e6d008d3
liangxiaodong <1027887088@qq.com>
2017-11-16 14:47:50 +0800

    增加更清爽

--- utils/attrExtract.py
+++ utils/attrExtract.py
@@ -2,7 +2,7 @@
 # @Author: xiaodong
 # @Date:   2017-11-16 14:05:58
 # @Last Modified by:   liangxiaodong
-# @Last Modified time: 2017-11-16 14:09:54
+# @Last Modified time: 2017-11-16 14:46:56
 def e(start='_', module='os'):
 	module = __import__(module)
 	def gen_attr():
@@ -11,6 +11,11 @@ def e(start='_', module='os'):
 				yield attr
 	yield from gen_attr()
 
+def e2(start='', module='os'):
+	module = __import__(module)
+	yield from (attr for attr in dir(module) if attr.startswith(start))
+
 
 if __name__ == '__main__':
 	print (list(e()))
+	print (list(e2('a')))
