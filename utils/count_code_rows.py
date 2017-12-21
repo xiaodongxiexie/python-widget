e1119cc8438cb9cae8b9ecdbb80bd520fecf4624
liangxiaodong <1027887088@qq.com>
2017-12-21 18:51:48 +0800

    增加统计指定文件夹内所有py文件代码量

--- utils/count_code_rows.py
+++ utils/count_code_rows.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # @Author: xiaodong
 # @Date:   2017-12-18 18:54:56
-# @Last Modified by:   xiaodong
-# @Last Modified time: 2017-12-18 19:05:10
+# @Last Modified by:   liangxiaodong
+# @Last Modified time: 2017-12-21 18:50:54
 
 '''
 简易统计Python代码行数
@@ -31,5 +31,20 @@ def count_code_nums(file):
     return count
 
 
+def detect_rows(begin=0, root='.'):
+    """
+    统计指定文件夹内所有py文件代码量
+    """
+    import os, glob
+    for file in glob.glob(os.path.join(root, '*')):
+        if os.path.isdir(file):
+            begin += detect_rows(0, file)
+        elif file.endswith('.py'):
+            begin += count_code_nums(file)
+    return begin
+
+
 if __name__ == '__main__':
     print (count_code_nums(__file__))
+    print (detect_rows(root=r'E:\Anaconda\Lib\site-packages\sklearn\metrics\tests'))
+
