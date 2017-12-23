b01e62c3c2cccc46354bba893264d8da14cb3c2f
liangxiaodong <1027887088@qq.com>
2017-12-23 11:12:07 +0800

    更新快排

--- algorithms/recursion.py
+++ algorithms/recursion.py
@@ -2,7 +2,7 @@
 # @Author: liangxiaodong
 # @Date:   2017-12-08 15:57:16
 # @Last Modified by:   liangxiaodong
-# @Last Modified time: 2017-12-08 16:29:59
+# @Last Modified time: 2017-12-08 16:32:34
 def sum(seq):
 	if seq == []:
 		return 0
@@ -30,6 +30,19 @@ def quick_sort(seq):
 		else:
 			right.append(ele)
 	return quick_sort(left) + [pivot] + quick_sort(right)
+
+def quicksort(array):
+  if len(array) < 2:
+    # base case, arrays with 0 or 1 element are already "sorted"
+    return array
+  else:
+    # recursive case
+    pivot = array[0]
+    # sub-array of all the elements less than the pivot
+    less = [i for i in array[1:] if i <= pivot]
+    # sub-array of all the elements greater than the pivot
+    greater = [i for i in array[1:] if i > pivot]
+    return quicksort(less) + [pivot] + quicksort(greater)
 
 if __name__ == '__main__':
 	seq = [1, 3, 5, 3, 4, 6]
