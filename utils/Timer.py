a4665d649e33e41fdd255323551740cf9f8d958a
liangxiaodong <1027887088@qq.com>
2018-01-12 15:14:32 +0800

    增加类装饰器

--- utils/Timer.py
+++ utils/Timer.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # @Author: xiaodong
 # @Date:   2017-12-04 13:28:45
-# @Last Modified by:   liangxiaodong
-# @Last Modified time: 2017-12-04 13:33:49
+# @Last Modified by:   xiaodong
+# @Last Modified time: 2018-01-12 15:11:11
 import time
 
 
@@ -16,9 +16,26 @@ class Timer(object):
 		self.cost = self.stop - self.start
 		return exc_type is None
 
+class TimerCost:
+	def __init__(self, function):
+		self.function = function
+
+	def __call__(self, *args, **kwargs):
+		start = time.time()
+		result = self.function(*args, **kwargs)
+		end = time.time()
+		print ('Total time: %s s' % (end-start))
+		return result
+
+@TimerCost
+def p(a, b=10):
+	time.sleep(2)
+	return a ** b
+
 if __name__ == '__main__':
 	def test():
 		time.sleep(10)
 	with Timer() as timer:
 		test()
 	print (timer.cost)
+	print (p(2))
