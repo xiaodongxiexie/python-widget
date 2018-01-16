ddbb9aa1ecd1187d5596839bfe5a061904e88975
liangxiaodong <1027887088@qq.com>
2018-01-16 13:18:36 +0800

    tuning

--- utils/retry.py
+++ utils/retry.py
@@ -2,34 +2,34 @@
 # @Author: xiaodong
 # @Date:   2018-01-16 11:52:08
 # @Last Modified by:   liangxiaodong
-# @Last Modified time: 2018-01-16 12:00:57
+# @Last Modified time: 2018-01-16 13:17:48
 from functools import wraps
 import traceback
 
-def retry(retries=3):
-	def _retry(func):
-		@wraps(func)
-		def _wrapper(*args, debug=False, **kwargs):
-			index = 0
-			result = None
-			while index < retries:
-				index += 1
-				try:
-					result = func(*args, **kwargs)
-					if result:
-						break
-				except Exception as e:
-					if debug:
-						traceback.print_exc()
-					else:pass
-			return result
-		return _wrapper
-	return _retry
+def retry(retries=3, debug=False):
+    def _retry(func):
+        @wraps(func)
+        def _wrapper(*args, **kwargs):
+            index = 0
+            result = None
+            while index < retries:
+                index += 1
+                try:
+                    result = func(*args, **kwargs)
+                    if result:
+                        break
+                except Exception as e:
+                    if debug:
+                        traceback.print_exc()
+                    else:pass
+            return result
+        return _wrapper
+    return _retry
 
 
 @retry(10)
 def pprint(i):
-	return 10 / i
+    return 10 / i
 
 
 import random
