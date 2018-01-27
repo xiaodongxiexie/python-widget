c2b7f12acb3c65baf0bee6f58f41b09198e2f746
liangxiaodong <1027887088@qq.com>
2018-01-28 00:03:21 +0800

    增加heatmap

--- python_import.py
+++ python_import.py
@@ -251,6 +251,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import seaborn  # 基于matplotlib的一个美化
 import folium  # 用于对地图可视化  pip install folium
+import heatmap  # 热力图
 
 # 图形化
 import PIL
