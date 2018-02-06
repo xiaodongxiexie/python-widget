4923b6d9b9dde2f783bf2584e5cad4691db15c50
liangxiaodong <1027887088@qq.com>
2018-02-06 17:43:32 +0800

    some_dangerous_operation

---
+++ utils/danger/dangerous.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Author: xiaodong
+# @Date:   2018-02-06 17:42:10
+# @Last Modified by:   xiaodong
+# @Last Modified time: 2018-02-06 17:42:34
+print().__class__.__bases__[0].__subclasses__()
