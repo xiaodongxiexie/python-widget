ba34c95942bf2b55d46f64f516404b1ad931bff4
liangxiaodong <1027887088@qq.com>
2018-02-26 10:44:14 +0800

    change fake_useragent

--- python_import.py
+++ python_import.py
@@ -280,7 +280,7 @@ import SimpleITK as sitk  # 图像处理
 import skimage
 
 # 爬虫
-#import fake - useragent  # pip install fake-useragent  生成伪装头部等信息
+#import fake_useragent  # pip install fake-useragent  生成伪装头部等信息
 import requests
 import grequests  # pip install grequests 一个利用gevent实现多线程的requests
 import lassie
