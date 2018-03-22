ac46ae61f20f5e0f7991390175e3954876e3dd31
liangxiaodong <1027887088@qq.com>
2018-03-22 17:18:06 +0800

    修复可变参数导致的问题

--- neural_net_draw.py
+++ neural_net_draw.py
@@ -2,13 +2,13 @@
 # @Author: xiaodong
 # @Date:   2018-03-22 14:05:27
 # @Last Modified by:   liangxiaodong
-# @Last Modified time: 2018-03-22 17:13:00
+# @Last Modified time: 2018-03-22 17:17:28
 
 import random
 import graphviz as gz
 
 
-def neural_graph(inp=3, hide=[10, 5, 6], outp=3, inp_label='input', hide_label='hide', outp_label='output', dropout=True, style='h', size='2, 1'):
+def neural_graph(inp=3, hide=(10, ), outp=3, inp_label='input', hide_label='hide', outp_label='output', dropout=True, style='h', size='2, 1'):
     """
     绘制简易神经网络图（有向图）
     :param inp: 输入神经元个数
@@ -36,7 +36,7 @@ def neural_graph(inp=3, hide=[10, 5, 6], outp=3, inp_label='input', hide_label='
                         dot.edge('%s%s' % (label1, i), '%s%s' % (label2, j))
                 else:
                     dot.edge('%s%s' % (label1, i), '%s%s' % (label2, j))
-
+    hide = list(hide)
     hide.insert(0, inp)
     hide.append(outp)
     for index, (i, j) in enumerate(zip(hide[:-1], hide[1:])):
