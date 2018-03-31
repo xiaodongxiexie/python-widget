cf28a23a44f1e3b8e5d51ef08beeff0f2239235a
liangxiaodong <1027887088@qq.com>
2018-03-31 15:53:59 +0800

    支持中文标点匹配

--- utils/regex/find_Chinese.py
+++ utils/regex/find_Chinese.py
@@ -9,7 +9,10 @@ import re
 if __name__ == "__main__":
 
     pat = re.compile('[\u4e00-\u9fa5]+')
+    pat2 = re.compile('[\u4e00-\u9fa5；，？。‘“”’]+')
+
     str_test = 'zhongguo China 中国， 世界， World, 长城'
 
     print(pat.findall(str_test))
+    print(pat2.findall(str_test))
 
