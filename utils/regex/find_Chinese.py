4956e9500420f7b174f51b85c231c81d27e504eb
liangxiaodong <1027887088@qq.com>
2018-03-31 15:50:12 +0800

    中文正则表达式

---
+++ utils/regex/find_Chinese.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# @Author: xiaodong
+# @Date:   just hide
+# @Last Modified by:   xiaodong
+# @Last Modified time: just hide
+import re
+
+
+if __name__ == "__main__":
+
+    pat = re.compile('[\u4e00-\u9fa5]+')
+    str_test = 'zhongguo China 中国， 世界， World, 长城'
+
+    print(pat.findall(str_test))
+
