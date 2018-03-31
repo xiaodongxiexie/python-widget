40af43d47f038a34900737394662d639c5a2960f
liangxiaodong <1027887088@qq.com>
2018-03-31 15:33:09 +0800

    update parse

--- utils/parse_datetime.py
+++ utils/parse_datetime.py
@@ -2,7 +2,7 @@
 # @Author: xiaodong
 # @Date:   2018-02-08 14:01:56
 # @Last Modified by:   liangxiaodong
-# @Last Modified time: 2018-02-08 14:02:34
+# @Last Modified time: no
 from datetime import datetime
 
 def parse_datetime(dt):
@@ -11,5 +11,12 @@ def parse_datetime(dt):
     return str(bytes(strd, encoding='utf-8').translate(table), encoding='utf-8')
 
 
+def parse_datetime2(dt):
+    strd = str(dt)
+    table = str.maketrans('-:.', '___')
+    return strd.translate(table)
+
+
 if __name__ == "__main__":
-	print(parse_datetime(datetime.now()))
+    print(parse_datetime(datetime.now()))
+    print(parse_datetime2(datetime.now()))
