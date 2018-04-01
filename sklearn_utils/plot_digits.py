6ed10f3cfde53a6504c50f3cb79abc62f908d70c
liangxiaodong <1027887088@qq.com>
2018-04-01 12:16:12 +0800

    add some attrs

--- sklearn_utils/plot_digits.py
+++ sklearn_utils/plot_digits.py
@@ -9,27 +9,40 @@ import numpy as np
 
 
 class Digits:
-    def __init__(self, nums):
+    def __init__(self, nums, shuffle=False):
         self.nums = nums
+        self.shuffle = shuffle
+
+        digits = load_digits(return_X_y=True)
+        self.x, self.y = digits
 
     def get_data(self):
-        digits = load_digits(return_X_y=True)
-        x, y = digits
+        x, y = self.x, self.y
         x = x[:self.nums]
         y = y[:self.nums]
         return x, y
 
+    def get_shuffle_data(self):
+        x, y = self.x, self.y
+        nums = np.random.permutation(self.nums)[:self.nums]
+        x, y = x.take(nums, axis=0), y.take(nums)
+        return x, y
+
+
 
 class Draw(Digits):
-    def __init__(self, nums, figsize=(12, 12)):
+    def __init__(self, nums, shuffle=False, figsize=(12, 12)):
         self.figsize = figsize
-        super().__init__(nums)
+        super().__init__(nums, shuffle=shuffle)
 
     def __call__(self):
         return self.draw()
 
     def draw(self):
-        x, y = self.get_data()
+        if self.shuffle:
+            x, y = self.get_shuffle_data()
+        else:
+            x, y = self.get_data()
 
         row, resid = divmod(self.nums, 8)
         if resid > 0:
@@ -48,5 +61,5 @@ class Draw(Digits):
 
 
 if __name__ == "__main__":
-    draw = Draw(164)
+    draw = Draw(17)
     draw()
