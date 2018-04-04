ea656482297bf78ee8d542148e76e4d224110ee2
liangxiaodong <1027887088@qq.com>
2018-04-04 14:47:07 +0800

    two styles to draw 3d cube

--- plot/plot3D_cube.py
+++ plot/plot3D_cube.py
@@ -15,18 +15,42 @@ plt.rcParams['axes.unicode_minus'] = False
 plt.rcParams['font.sans-serif'] = ['SimHei']
 
 
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1, projection='3d')
+def plot_opaque_cube(x=0, y=0, z=0, dx=10, dy=10, dz=10):
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1, projection='3d')
 
 
-x = np.linspace(0, 10, 100)
-y = np.linspace(0, 10, 100)
-xx, yy = np.meshgrid(x, y)
-for a, b, c in permutations([xx, yy, 0], 3):
-    ax.plot_surface(a, b, c)
-for a, b, c in permutations([xx, yy, 10], 3):
-    ax.plot_surface(a, b, c)
-ax.set_xlim3d(-10, 20, 20)
-ax.set_zlim3d(-10, 20, 20)
-ax.set_ylim3d(-10, 20, 20);
-plt.show()
+    x = np.linspace(x, x+dx, 5)
+    y = np.linspace(y, y+dy, 5)
+    xx, yy = np.meshgrid(x, y)
+    for a, b, c in permutations([xx, yy, 0], 3):
+        ax.plot_surface(a, b, c)
+    for a, b, c in permutations([xx, yy, 10], 3):
+        ax.plot_surface(a, b, c)
+    # ax.set_xlim3d(-dx, dx*2, 20)
+    # ax.set_xlim3d(-dx, dx*2, 20)
+    # ax.set_xlim3d(-dx, dx*2, 20)
+    plt.title("Cube")
+    plt.show()
+
+
+
+def plot_linear_cube(x, y, z, dx, dy, dz, color='red'):
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    xx = [x, x, x+dx, x+dx, x]
+    yy = [y, y+dy, y+dy, y, y]
+    kwargs = {'alpha': 1, 'color': color}
+    ax.plot3D(xx, yy, [z]*5, **kwargs)
+    ax.plot3D(xx, yy, [z+dz]*5, **kwargs)
+    ax.plot3D([x, x], [y, y], [z, z+dz], **kwargs)
+    ax.plot3D([x, x], [y+dy, y+dy], [z, z+dz], **kwargs)
+    ax.plot3D([x+dx, x+dx], [y+dy, y+dy], [z, z+dz], **kwargs)
+    ax.plot3D([x+dx, x+dx], [y, y], [z, z+dz], **kwargs)
+    plt.title('Cube')
+    plt.show()
+
+
+if __name__ == "__main__":
+    plot_linear_cube(0, 0, 0, 100, 120, 130)
+    plot_opaque_cube()
