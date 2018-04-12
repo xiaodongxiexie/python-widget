2512880b1a6be50a69d23c33c4f84f1a9d1aa64b
liangxiaodong <1027887088@qq.com>
2018-04-12 15:35:46 +0800

    map for numpy

---
+++ numpy_utils/arr_map.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# @Author: xiaodong
+# @Date:   just hide
+# @Last Modified by:   xiaodong
+# @Last Modified time: just hide
+import numpy as np
+
+from typing import Callable
+
+
+def arr_map(arr: np.array, function: Callable) -> np.array:
+    shape = arr.shape
+    arr = np.array(list(map(function, arr.ravel()))).reshape(shape)
+    return arr
+
+
+if __name__ == "__main__":
+    arr = np.random.randn(3,4,3)
+
+    arr2int = arr_map(arr, int)
+    arr_plus_10 = arr_map(arr, lambda x: x+10)
+    print(arr2int)
+    print(arr_plus_10)
