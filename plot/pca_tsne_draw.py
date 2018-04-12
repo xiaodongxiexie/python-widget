ede2be72ef7c8e45797c97c3a6f5a72e8e3d44af
liangxiaodong <1027887088@qq.com>
2018-04-12 09:42:44 +0800

    add __main__

--- plot/pca_tsne_draw.py
+++ plot/pca_tsne_draw.py
@@ -42,32 +42,34 @@ def plot_embedding(X, title=None):
     if title is not None:
         plt.title(title)
 
-#‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐
-# Plot images of the digits
-n_img_per_row = 20
-img = np.zeros((10 * n_img_per_row, 10 * n_img_per_row))
-for i in range(n_img_per_row):
-    ix = 10 * i + 1
-    for j in range(n_img_per_row):
-        iy = 10 * j + 1
-        img[ix:ix + 8, iy:iy + 8] = X[i * n_img_per_row + j].reshape((8, 8))
-        plt.imshow(img, cmap=plt.cm.binary)
-        plt.xticks([])
-        plt.yticks([])
-        plt.title('A selection from the 64‐dimensional digits dataset')
-# Computing PCA
-print("Computing PCA projection")
-t0 = time()
-X_pca = decomposition.TruncatedSVD(n_components=2).fit_transform(X)
-plot_embedding(X_pca,
-               "Principal Components projection of the digits (time %.2fs)" %
-               (time() - t0))
-# Computing t‐SNE
-print("Computing t‐SNE embedding")
-tsne = manifold.TSNE(n_components=2, init='pca', random_state=0)
-t0 = time()
-X_tsne = tsne.fit_transform(X)
-plot_embedding(X_tsne,
-               "t‐SNE embedding of the digits (time %.2fs)" %
-               (time() - t0))
-plt.show()
+
+if __name__ == "__main__":
+    #‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐
+    # Plot images of the digits
+    n_img_per_row = 20
+    img = np.zeros((10 * n_img_per_row, 10 * n_img_per_row))
+    for i in range(n_img_per_row):
+        ix = 10 * i + 1
+        for j in range(n_img_per_row):
+            iy = 10 * j + 1
+            img[ix:ix + 8, iy:iy + 8] = X[i * n_img_per_row + j].reshape((8, 8))
+            plt.imshow(img, cmap=plt.cm.binary)
+            plt.xticks([])
+            plt.yticks([])
+            plt.title('A selection from the 64‐dimensional digits dataset')
+    # Computing PCA
+    print("Computing PCA projection")
+    t0 = time()
+    X_pca = decomposition.TruncatedSVD(n_components=2).fit_transform(X)
+    plot_embedding(X_pca,
+                   "Principal Components projection of the digits (time %.2fs)" %
+                   (time() - t0))
+    # Computing t‐SNE
+    print("Computing t‐SNE embedding")
+    tsne = manifold.TSNE(n_components=2, init='pca', random_state=0)
+    t0 = time()
+    X_tsne = tsne.fit_transform(X)
+    plot_embedding(X_tsne,
+                   "t‐SNE embedding of the digits (time %.2fs)" %
+                   (time() - t0))
+    plt.show()
