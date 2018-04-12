1b93e4077cf2fe590ebbd6adffa359bac329102f
liangxiaodong <1027887088@qq.com>
2018-04-12 16:42:33 +0800

    beautiful show content

---
+++ utils/gentle_show.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# @Author: xiaodong
+# @Date:   just hide
+# @Last Modified by:   xiaodong
+# @Last Modified time: just hide
+
+
+def gentle_show(seq, *, column=4):
+    seq = list(map(str, seq))
+    max_len = len(max(seq, key=len))
+
+    for index, ele in enumerate(seq):
+        if index % column == 0:
+            print('-' * max_len * column + '-' * (column - 1))
+            print(ele.center(max_len, ' '), end='|')
+        else:
+            if (index - column + 1) % column == 0:
+                print(ele.center(max_len, ' '))
+            else:
+                print(ele.center(max_len, ' '), end='|')
+
+if __name__ == "__main__":
+    gentle_show(dir([]), column=6)
