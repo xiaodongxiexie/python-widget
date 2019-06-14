385555f39077b07ea18b2d4dbb026ee3a2c6733f
liangxiaodong <1027887088@qq.com>
2019-06-14 18:21:17 +0800

    secret

---
+++ PythonWidget/utils/secret.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# @Author: xiaodong
+# @Date:   just hide
+# @Last Modified by:   xiaodong
+# @Last Modified time: just hide
+class Secret:
+    """
+    Holds a string value that should not be revealed in tracebacks etc.
+    You should cast the value to `str` at the point it is required.
+    """
+
+    def __init__(self, value: str):
+        self._value = value
+
+    def __repr__(self) -> str:
+        class_name = self.__class__.__name__
+        return f"{class_name}('**********')"
+
+    def __str__(self) -> str:
+        return self._value
