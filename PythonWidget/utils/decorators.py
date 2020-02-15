e872e68c32f52fd9b7e614fd3b6e73aaddf2e83d
暁东邪 <xiaodongxiexie@users.noreply.github.com>
2020-02-15 11:10:45 +0800

    Update decorators.py

--- PythonWidget/utils/decorators.py
+++ PythonWidget/utils/decorators.py
@@ -262,3 +262,31 @@ class Elapsed:
             return self
         else:
             return types.MethodType(self, instance)
+        
+ 
+from functools import wraps, partial
+import logging
+
+def logged(func=None, *, level=logging.DEBUG, name=None, message=None):
+    if func is None:
+        return partial(logged, level=level, name=name, message=message)
+
+    logname = name if name else func.__module__
+    log = logging.getLogger(logname)
+    logmsg = message if message else func.__name__
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        log.log(level, logmsg)
+        return func(*args, **kwargs)
+
+    return wrapper
+
+# Example use
+@logged
+def add(x, y):
+    return x + y
+
+@logged(level=logging.CRITICAL, name='example')
+def spam():
+    print('Spam!')
