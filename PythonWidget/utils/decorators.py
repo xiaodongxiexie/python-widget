43a804a6099c5b56676ff7cc12639895e1aa73e7
暁东邪 <xiaodongxiexie@users.noreply.github.com>
2020-05-12 20:33:21 +0800

    lazy property

--- PythonWidget/utils/decorators.py
+++ PythonWidget/utils/decorators.py
@@ -290,3 +290,32 @@ def add(x, y):
 @logged(level=logging.CRITICAL, name='example')
 def spam():
     print('Spam!')
+
+
+
+_missing = object()
+
+class locked_cached_property(object):
+    """A decorator that converts a function into a lazy property.  The
+    function wrapped is called the first time to retrieve the result
+    and then that calculated result is used the next time you access
+    the value.  Works like the one in Werkzeug but has a lock for
+    thread safety.
+    """
+
+    def __init__(self, func, name=None, doc=None):
+        self.__name__ = name or func.__name__
+        self.__module__ = func.__module__
+        self.__doc__ = doc or func.__doc__
+        self.func = func
+        self.lock = RLock()
+
+    def __get__(self, obj, type=None):
+        if obj is None:
+            return self
+        with self.lock:
+            value = obj.__dict__.get(self.__name__, _missing)
+            if value is _missing:
+                value = self.func(obj)
+                obj.__dict__[self.__name__] = value
+            return value
