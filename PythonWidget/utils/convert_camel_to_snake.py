58fd929ee57884a73a1d586c7b891c82b9727f93
暁东邪 <xiaodongxiexie@users.noreply.github.com>
2023-03-23 10:17:21 +0800

    Update convert_camel_to_snake.py

--- PythonWidget/utils/convert_camel_to_snake.py
+++ PythonWidget/utils/convert_camel_to_snake.py
@@ -3,3 +3,7 @@ import re
 
 def convert_camel_to_snake(camel_str):
     return re.sub('(?!^)([A-Z]+)', r'_\1', camel_str).lower()
+
+
+ef camel_to_snake(name: str) -> str:
+    return re.sub(r'(?<!^)(?=[A-Z])', '_', name).lower()
